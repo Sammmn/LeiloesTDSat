aebdcda33df501628dcc5023dd758e9ee34d3428
Samuel Marques <samuel.marques414@gmail.com>
2023-12-16 16:36:12 -0300

    Teste

--- LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
+//teste
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
---
+++ LeiloesTDSat/README.md
@@ -0,0 +1,7 @@
+# Projeto  leilões do curso de TDS (técnico em desenvolvimento de sistemas)
+
+## Descrição do projeto:
+O projeto consiste em uma solução para automatizar os processos de uma casa de leilões, optimizando assim as atividades inerentes desenvolvidas pela mesma.
+
+## Tecnologias utilizadas:
+Java e MySQL
